0b6581a25418483dff3dd0a2605db370cf318944
Vlad <Vlad@LAPTOP-LRI5NNV3>
2023-09-22 09:59:16 +0300

    Добавлено наполнение таблиц данными

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TestTask.Models;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace TestTask.Controllers
 {
@@ -11,6 +12,27 @@ namespace TestTask.Controllers
         public HomeController(ApplicationContext context)
         {
             db = context;
+            ///<summary>Наполняю таблицы разными данными </summary>
+            if (!db.Categories.Any())
+            {
+                Category food = new Category { Name = "Еда" };
+                Category electronic = new Category { Name = "Электроника" };
+                Category cosmetics = new Category { Name = "Косметика" };
+                Category stationery = new Category { Name = "Канцтовары" };
+
+                Product Product1 = new Product { Name = "Мороженное", Category = food };
+                Product Product2 = new Product { Name = "Тирамису", Category = food };
+                Product Product3 = new Product { Name = "Телевизор", Category = electronic };
+                Product Product4 = new Product { Name = "Утюг", Category = electronic };
+                Product Product5 = new Product { Name = "Помада", Category = cosmetics };
+                Product Product6 = new Product { Name = "Крем для рук", Category = cosmetics };
+                Product Product7 = new Product { Name = "Ручка шариковая", Category = stationery };
+                Product Product8 = new Product { Name = "Линейка", Category = stationery };
+
+                db.Categories.AddRange(food, electronic, cosmetics, stationery);
+                db.Products.AddRange(Product1, Product2, Product3, Product4, Product5, Product6, Product7, Product8);
+                db.SaveChanges();
+            }
         }
         /// <summary>Вывод на web-страницу данных двух таблиц</summary>
         public async Task<IActionResult> Index()
